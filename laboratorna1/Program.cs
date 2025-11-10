74d395e8fd45899bc68be0ab55484713d9cdc5d3
alfoae <vzhe.zanjato@gmail.com>
2025-11-11 00:49:25 +0300

    laboratorna2

--- laboratorna1/Program.cs
+++ laboratorna1/Program.cs
@@ -1,67 +1,75 @@
-﻿using System;
+﻿using static tovar;
 
-class Program
+class menu
 {
     static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("=== СЕРВІС ПРОКАТУ АВТО ===\n");
-        Console.ResetColor();
+        string i;
+        short num = 0;
+        do
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("=== ЛАСКАВО ПРОСИМО ДО СЕРВІСУ ПРОКАТУ АВТО ===\n");
+            Console.ResetColor();
 
-        double zhiguli = 700;
-        double uaz = 1200;
-        double buhanka = 1000;
-        double corolla = 1800;
 
-        Console.WriteLine("1. Жигулі – 700 грн/доба");
-        Console.WriteLine("2. УАЗ – 1200 грн/доба");
-        Console.WriteLine("3. Буханка – 1000 грн/доба");
-        Console.WriteLine("4. Тойота – 1800 грн/доба\n");
 
-        Console.Write("Введіть кількість днів оренди для Жигулі: ");
-        int d1 = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Функція в розробці виберіть щось інше (1)\n");
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Ви ввели щось не то :/ спробуйте вибрати від 0 до 4\n");
+                    break;
+                case 0:
+                    Console.Write("виберіть що вам потрібно\n");
+                    break;
+            }
 
-        Console.Write("Введіть кількість днів оренди для УАЗ: ");
-        int d2 = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1. перегляд товарів\n");
+            Console.WriteLine("2. розрахунок покупки\n");
+            Console.WriteLine("3. інформація про магазин\n");
+            Console.WriteLine("4. налаштування\n");
+            Console.WriteLine("0. вихід\n");
+            i = Console.ReadLine();
+            switch (i)
+            {
+                case "1":
+                    Console.Clear();
+                    tovaro();
+                    break;
+                case "2":
+                    Console.Clear();
+                    num = 1;
+                    break;
+                case "3":
+                    Console.Clear();
+                    num = 1;
+                    break;
+                case "4":
+                    Console.Clear();
+                    num = 1;
+                    break;
+                case "0":
+                    Console.Clear();
+                    break;
+                default:
+                    Console.Clear();
+                    num = 2;
+                    break;
+            }
+        } while (i != "0");
 
-        Console.Write("Введіть кількість днів оренди для Буханки: ");
-        int d3 = int.Parse(Console.ReadLine());
 
-        Console.Write("Введіть кількість днів оренди для Corolla: ");
-        int d4 = int.Parse(Console.ReadLine());
 
-        double s1 = zhiguli * d1;
-        double s2 = uaz * d2;
-        double s3 = buhanka * d3;
-        double s4 = corolla * d4;
 
-        double total = s1 + s2 + s3 + s4;
-
-        Random rnd = new Random();
-        double discount = rnd.Next(0, 11);
-        double discountAmount = total * discount / 100;
-        double finalTotal = total - discountAmount;
-
-        finalTotal = Math.Round(finalTotal, 2);
-        double sqrtBonus = Math.Sqrt(finalTotal);
-
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("\n=== РЕЗУЛЬТАТИ ===");
-        Console.ResetColor();
-
-        Console.WriteLine($"Жигулі: {s1} грн");
-        Console.WriteLine($"УАЗ: {s2} грн");
-        Console.WriteLine($"Буханка: {s3} грн");
-        Console.WriteLine($"Тойота: {s4} грн");
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"\nЗагальна сума без знижки: {total} грн");
-        Console.WriteLine($"Знижка: {discount}% (-{discountAmount} грн)");
-        Console.WriteLine($"Підсумкова сума до оплати: {finalTotal} грн");
-        Console.ResetColor();
-
-        Console.WriteLine($"\n(√ із суми для перевірки: {Math.Round(sqrtBonus, 2)})");
-        Console.WriteLine("\nДякуємо, що обрали наш гаражний сервіс прокату!");
+        Console.WriteLine("\nДякуємо, що скористалися нашим сервісом!");
+        Console.WriteLine("Натисніть будь-яку клавішу для виходу...");
+        Console.ReadKey();
     }
 }
